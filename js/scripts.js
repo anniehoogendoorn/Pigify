339313a3d2dc26bbaff46d2568cfbf39ab7027fa
John <student@epicodus.com>
2015-09-09 16:55:00 -0700

    Refactor pigify

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,21 @@
-var pigify = function(string) {
+var pigify = function(word) {
 
-    for (var i=0; i < string.length; i++) {
-        if ((string[i] === 'a') ||
-            (string[i] === 'e') ||
-            (string[i] === 'i') ||
-            (string[i] === 'o') ||
-            (string[i] === 'u')) {
-                if(string[i-1] === 'q') {
-                    var slice_point = i+1;
-                } else {
-                    var slice_point = i;
-                }
-                var first_letters = string.slice(0, slice_point);
-                var remainder = string.slice(slice_point, string.length);
-                var piggy = remainder + first_letters + 'ay';
-                return piggy;
-        }
+  var vowels = ["a", "e", "i", "o", "u"];
 
+  for (var i=0; i < word.length; i++) {
+    if ((vowels.indexOf(word[i])) != -1) {
+
+      if ((word[i] === "u") && (word[i-1] === "q")) {
+        var slice_point = i+1;
+      } else {
+        var slice_point = i;
+      }
+
+      var first_letters = word.slice(0, slice_point);
+      var remainder = word.slice(slice_point, word.length);
+      var piggy = remainder + first_letters + 'ay';
+
+      return piggy;
     }
+  }
 };
-
-
-//move all letters up to first vowel to end of word, add 'ay'
-
-//if first vowel is a u, check for a q before it and move the pair
-
-
-
-
-$(document).ready(function() {
-
-});
